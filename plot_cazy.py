527173d2fbc466027f8d81dd99b0fe2b97000ae8
sy520 <36355222+zxgsy520@users.noreply.github.com>
2021-11-23 23:40:11 +0800

    Add files via upload

--- plot_cazy.py
+++ plot_cazy.py
@@ -12,9 +12,9 @@ from matplotlib import pyplot as plt
 
 LOG = logging.getLogger(__name__)
 
-__version__ = "0.1.0"
+__version__ = "1.1.0"
 __author__ = ("Xingguo Zhang",)
-__email__ = "1131978210@qq.com"
+__email__ = "invicoun@foxmail.com"
 __all__ = []
 
 
@@ -44,41 +44,43 @@ def read_cazy_classify(file):
 
     for line in read_tsv(file, '\t'):
         family.append(line[0])
-        number.append(int(line[2]))
-        func.append('%s:%s (%s)' % (line[0], line[3], line[2]))
+        number.append(int(line[1]))
+        func.append('%s:%s (%s)' % (line[0], line[2], line[1]))
 
     return family, number, func
 
 
-def plot_cazy(family, number, func, name):
+def plot_cazy(family, number, func, prefix):
 
-    colors = ['#FF4500', '#ADFF2F', '#9ACD32', '#00FFFF', '#0000FF', '#FF00FF']
+    # colors = ['#FF4500', '#ADFF2F', '#9ACD32', '#00FFFF', '#0000FF', '#FF00FF']
+    colors = ["#0e72cc", "#6ca30f", "#f59311", "#fa4343", "#16afcc", "#85c021",
+              "#d12a6a", "#0e72cc", "#6ca30f", "#f59311", "#fa4343", "#16afcc"]
     font1 = {'family': 'Times New Roman',
          'weight': 'normal',
-         'size': 17,
+         'size': 14,
          }
     font2 = {'family': 'Times New Roman',
          'weight': 'normal',
-         'size': 18,
+         'size': 14,
          }
 
     plt.switch_backend('agg')
-    fig = plt.figure(figsize=[14.5, 7])
+    fig = plt.figure(figsize=[13.5, 7])
 
-    ax = plt.axes([0.10, 0.08, 0.525, 0.88])
+    ax = plt.axes([0.08, 0.08, 0.575, 0.88])
     ax.spines['bottom'].set_linewidth(2)
     ax.spines['left'].set_linewidth(2)
     ax.spines['top'].set_linewidth(2)
     ax.spines['right'].set_linewidth(2)
-    ax.tick_params(axis='x', length=0, labelsize=16)
-    ax.tick_params(axis='y', length=6, width=2, labelsize=16)
+    ax.tick_params(axis='x', length=0, labelsize=14)
+    ax.tick_params(axis='y', length=6, width=2, labelsize=14)
     x = range(len(family))
-    ax.bar(x, number, width = 0.5, linewidth=1.5, color = colors[0:len(family)], edgecolor='#000000')
+    ax.bar(x, number, width = 0.7, linewidth=1.5, color = colors[0:len(family)], edgecolor='#000000')
     plt.xticks(x, family)
     plt.ylim([0, max(number)+5])
     ax.set_ylabel('Number of sequences', font1)
     
-    axt = plt.axes([0.635, 0.08, 0.32, 0.88])
+    axt = plt.axes([0.67, 0.08, 0.85, 0.88])
     axt.axis('off')
     axt.spines['top'].set_visible(False)
     axt.spines['right'].set_visible(False)
@@ -86,19 +88,19 @@ def plot_cazy(family, number, func, name):
     axt.spines['left'].set_visible(False)
     n = 0.85
     for i in func:
-        n = n -0.1
+        n = n-0.06
         axt.text(0, n, i, font2)
 
-    plt.savefig('%s.cazy.pdf' % name)
-    plt.savefig('%s.cazy.png' % name, dpi=700)
+    plt.savefig('%s.cazy.pdf' % prefix)
+    plt.savefig('%s.cazy.png' % prefix, dpi=700)
 
 
 def add_help(parser):
 
-    parser.add_argument('-i', '--input', metavar='FILE', type=str, required=True,
+    parser.add_argument('input', metavar='FILE', type=str,
         help='Input CAZy database annotation classification file, CAZy_classify.tsv')
-    parser.add_argument('-n', '--name', metavar='STR', type=str, default='out',
-        help='The name of the output file.')
+    parser.add_argument('-p', '--prefix', metavar='STR', type=str, default='out',
+        help='The prefix of the output file.')
 
     return parser
 
@@ -116,7 +118,7 @@ name:
     plot_cazy.py Draw an annotated columnar distribution chart of the CAZy database
 
 attention:
-    plot_cazy.py -i CAZy_classify.tsv -n name
+    plot_cazy.py -i CAZy_classify.tsv -p prefix
 
 version: %s
 contact:  %s <%s>\
@@ -125,7 +127,7 @@ contact:  %s <%s>\
     args = add_help(parser).parse_args()
     family, number, func = read_cazy_classify(args.input)
 
-    plot_cazy(family, number, func, args.name)
+    plot_cazy(family, number, func, args.prefix)
 
 if __name__ == "__main__":
 
